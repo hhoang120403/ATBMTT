aab0d6a681eecc071eab3faa44d02da13370dfcc
hhoang_1204 <101880686+hhoang120403@users.noreply.github.com>
2024-04-08 23:40:00 +0700

    Create Vigenere_AutoKey.cpp

---
+++ Vigenere_AutoKey.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+string repeatKey(string input, string key){
+	string newKey = key; 
+	int length = input.length();
+	
+	while(newKey.length() < length){
+		for(int i = 0 ; i < length; i++){		
+			newKey += input[i];
+			if(newKey.length() == length) break;
+		}
+	} 
+	
+	return newKey; 
+} 
+
+int position(char c){
+	if(c > 'Z') return c - 'a';
+	return c - 'A';
+} 
+
+string VigenereEncryption(string input, string key){
+	string cipher = "";
+	int p, k, c, m = key.length();
+	
+	for(int i = 0; i < input.length(); i++){
+		p =  position(input[i]);
+		k = position(key[i%m]);
+		c = (p+k) % 26;
+		cipher += alphabet[c];
+	} 
+	
+	return cipher;
+}
+
+int main()
+{
+	string input, k, key;
+	int length;
+	
+	cout << "Nhap input: ";
+	cin >> input;
+	cout << "Nhap key: ";
+	cin >> key;
+	
+	cout << "Input: " << input << "\nKey: " << key << endl;
+	
+	string newKey;
+	newKey = repeatKey(input, key);
+	
+	cout << "New Key: " << newKey << endl;
+	
+	string res = VigenereEncryption(input, newKey);
+	
+	cout << "Vigenere Encryption: " << res << endl;
+}
